3e85163b6905e25114ec922574c4384df6ee2a9f
amandafd <amanda_dionisio@hotmail.com>
2022-12-03 00:06:32 -0300

    feat: adding event listener capturing city typed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,12 @@ const apiKey = "8caa3e1b07663a00accf703a779c01e1";
 const apiCountryURL = "http://countryflagsapi.com/png/";
 
 const cityInput = document.querySelector("#city-input");
-const searchBtn = document.querySelector("search");
+const searchBtn = document.querySelector("#search");
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    console.log(city);
+})
