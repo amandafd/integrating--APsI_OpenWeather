50e3ca5a87d2c45a7dc580a8ebe259caa6671755
amandafd <amanda_dionisio@hotmail.com>
2022-12-04 19:38:43 -0300

    feat: getting datas

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,32 @@ const apiCountryURL = "http://countryflagsapi.com/png/";
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
+const cityElement = document.querySelector('#city');
+const tempElement = document.querySelector('#temperature span');
+const descElement = document.querySelector('#description');
+const weatherIconElement = document.querySelector('#weather-icon');
+const countryElement = document.querySelector('#country');
+const umidityElement = document.querySelector('#umidity span');
+const windElement = document.querySelector('#wind span');
+
+
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=eng`;
+
+    const res = await  fetch(apiWeatherURL);
+    const data = await res.json();
+
+    console.log(data);
+}
+
+const showWeatherData = (city) => {
+    getWeatherData(city);
+}
+
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
     const city = cityInput.value;
 
-    console.log(city);
+    showWeatherData(city);
 })
